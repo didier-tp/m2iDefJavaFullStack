00a3409d57ece4901dcce3d337377e0e0d31a87e
didier <bob@example.com>
2019-03-01 15:27:39 +0100

    v1 jquery

--- serveurRestSpringMvc/src/main/resources/static/js/produit.js
+++ serveurRestSpringMvc/src/main/resources/static/js/produit.js
@@ -1,7 +1,20 @@
 $(function() {
 	
 	$('#btnRechercherParNum').on('click',function(){
-		$('#resProd').html('click');
+		var numProd = $('#numProd').val();
+		console.log("./rest/produit/" + numProd)
+		$.ajax({
+			type: "GET",
+			url: "./rest/produit/" + numProd ,
+			success: function (data,status,xhr) {
+					var jsonString = JSON.stringify(data);
+		            $('#resProd').html(jsonString);
+			},
+			error: function( jqXHR, textStatus, errorThrown ){
+				console.log(textStatus);
+			}
+		});
+		
 	});
 	
 	
