d63dc40bb130dbed2696c541dd105993af5bd8b3
didier <bob@example.com>
2019-03-01 10:19:14 +0100

    avec debut ProduitRestCtrl

--- serveurRestSpringMvc/src/main/java/com/m2i/tp/rest/ProduitRestCtrl.java
+++ serveurRestSpringMvc/src/main/java/com/m2i/tp/rest/ProduitRestCtrl.java
@@ -1,6 +1,11 @@
 package com.m2i.tp.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m2i.tp.entity.Produit;
@@ -9,8 +14,17 @@ import com.m2i.tp.entity.Produit;
 @RequestMapping(value="/rest/produit" , headers="Accept=application/json")
 public class ProduitRestCtrl {
 	
-	Produit getProduitByNum(Long numero){
-		return null;
+	private Map<Long,Produit> mapProduits = new HashMap<>();
+	
+	public ProduitRestCtrl() {
+		mapProduits.put(1L, new Produit(1L,"produit 1" , 12.0));
+		mapProduits.put(2L, new Produit(2L,"produit 2" , 56.0));
+		mapProduits.put(3L, new Produit(3L,"produit 3" , 28.0));
+	}
+	//URL = http://localhost:8080/serveurRestSpringMvc/rest/produit/1
+	@RequestMapping(value="/{numProd}" , method=RequestMethod.GET)
+	public Produit getProduitByNum(@PathVariable("numProd")  Long numero){
+		return mapProduits.get(numero);
 	}
 
 }
