abc284dc16c2579dca0ece5c1e12936f76abab0d
didier <bob@example.com>
2019-03-05 11:40:04 +0100

    avec design pattern stratégie

--- tpSpringCore/src/main/java/com/m2i/tp/essai/MyCtrlApp.java
+++ tpSpringCore/src/main/java/com/m2i/tp/essai/MyCtrlApp.java
@@ -3,8 +3,8 @@ package com.m2i.tp.essai;
 import java.util.Scanner;
 
 public class MyCtrlApp {
-	
-	public void sequence() {
+	/*
+	public void sequenceV1SansPattern() {
 		//1 saisir x
 		double x;
 		Scanner s = new Scanner(System.in);
@@ -14,6 +14,29 @@ public class MyCtrlApp {
 		double y = Math.sqrt(x);
 		//3 afficher le résultat
 		System.out.println("y="+y);
+	}*/
+	
+	private Calculateur calculateur=null;
+	
+	private Afficheur afficheur=null;
+	
+	
+	public void sequence() {
+		//on délègue vers sous objets interchangeables 
+		//Design pattern STRATEGIE .
+		
+		//calculateur = new CalculateurV1();
+		calculateur = new CalculateurV2();
+		//afficheur = new AfficheurModeTexte();
+		afficheur = new AfficheurGraphique();
+		
+		//1 saisir x
+		double x;
+		x=afficheur.saisir("x:");
+		//2 calculer racine carrée
+		double y = calculateur.racineCarree(x);
+		//3 afficher le résultat
+		afficheur.afficher("y="+y);
 	}
 
 	public static void main(String[] args) {
