f673022b88548fa9ef9d316ec3afa1873d2176e9
didier-tp <bob@example.com>
2019-03-11 12:32:01 +0100

    avec imports techniques dans app.module.ts

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
+import { ProduitService } from 'src/app/produit.service';
 
 @NgModule({
   declarations: [
@@ -11,9 +13,9 @@ import { ProduitComponent } from './produit/produit.component';
     ProduitComponent
   ],
   imports: [
-    BrowserModule , FormsModule
+    BrowserModule , FormsModule , HttpClientModule
   ],
-  providers: [],
+  providers: [ProduitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
