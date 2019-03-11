02ef33cf727ea5b4592a318166c818304e43b160
didier-tp <bob@example.com>
2019-03-11 12:25:19 +0100

    avec appel ws rest

--- angular-app/src/app/produit.service.ts
+++ angular-app/src/app/produit.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Produit } from 'src/app/produit';
 import { Observable , of} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProduitService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   public rechercherProduit$(prixMaxi : number) : Observable<Produit[]> {
-    //version preliminaire (simulation)
+  /*  //version preliminaire (simulation)
      let tabProduit = [
        { numero : 1 , label : "produit 1" , prix : prixMaxi -1 } ,
        { numero : 2 , label : "produit 2" , prix : prixMaxi }
      ]
      return of(tabProduit);
+     */
+     let wsUrl = "http://localhost:8080/serveurRestSpringMvc/"
+             + "rest/produit?prixMaxi=" + prixMaxi;
+    return this.http.get<Produit[]>(wsUrl );
   }
 
 }
