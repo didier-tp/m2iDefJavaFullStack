7c36b7865b34792c46e998b934e892305256426a
didier-tp <bob@example.com>
2019-03-18 10:20:56 +0100

    debut pwa avec angular

---
+++ pwa/avec_angular/angular-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NewsComponent } from './news/news.component';
+import { ConvComponent } from './conv/conv.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsComponent,
+    ConvComponent
+  ],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
